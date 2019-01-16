d2802c49e2fd30063141a3ed75cb647ba0bd9fc4
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2019-01-16 17:48:33 -0500

    update cli name

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# CliApp
+# First Swift App
 
-A description of this package.
+This is my first attempt in creating anything using Swift 4.2 for a binary command line application.
+
+#Results
+
+[![asciicast](https://asciinema.org/a/qllJZJvSMXQpk3A5MqhfvFbnG.svg)](https://asciinema.org/a/qllJZJvSMXQpk3A5MqhfvFbnG)
--- Sources/CliApp/main.swift
+++ Sources/CliApp/main.swift
@@ -1,7 +1,7 @@
 import SwiftCLI
 
 
-let cli = CLI(name: "igdb", version: "1.0.0", description: "iGDB client API")
+let cli = CLI(name: "bomb", version: "1.0.0", description: "GiantBomb client API")
 let manager = NetworkManager()
 cli.commands = [AuthCommand(), PlatformCommand(manager: manager), GameCommand(manager: manager)]
 cli.goAndExit()
